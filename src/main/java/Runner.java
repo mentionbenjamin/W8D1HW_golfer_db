234bd6a43007c8f028e6d2ef0d7700561845b677
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-10 06:37:41 +0100

    find by id adjusted

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -8,16 +8,14 @@ public class Runner {
     public static void main(String[] args) {
         Golfer golfer1 = new Golfer("Tiger", "Woods", 42);
         DBGolfer.save(golfer1);
-        Golfer golfer2 = new Golfer("Jordan", "Speith", 31);
+        Golfer golfer2 = new Golfer("Jordan", "Speith", 24);
         DBGolfer.save(golfer2);
-        Golfer golfer3 = new Golfer("Phil", "Michelson", 51);
+        Golfer golfer3 = new Golfer("Phil", "Michelson", 48);
         DBGolfer.save(golfer3);
 
         List<Golfer> Golfers = DBGolfer.getAll();
 
-        Golfer davey = DBGolfer.find(golfer3.getId());
-
-        System.out.println(davey.getFirstName());
+        Golfer phil = DBGolfer.find(golfer3.getId());
 
         System.exit(0);
 
