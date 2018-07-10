f5e48459029f601578e1d1acfca2499c0c5ac7cd
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-10 06:29:47 +0100

    set up runner with data and two crud functions

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,2 +1,25 @@
+import db.DBGolfer;
+import models.Golfer;
+
+import java.util.List;
+
 public class Runner {
+
+    public static void main(String[] args) {
+        Golfer Golfer1 = new Golfer("Tiger", "Woods", 42);
+        DBGolfer.save(golfer1);
+        Golfer Golfer2 = new Golfer("Jordan", "Speith", 31);
+        DBGolfer.save(golfer2);
+        Golfer Golfer3 = new Golfer("Phil", "Michelson", 51);
+        DBGolfer.save(golfer3);
+
+        List<Golfer> Golfers = DBGolfer.getAll();
+
+        Golfer davey = DBGolfer.find(golfer3.getId());
+
+        System.out.println(davey.getFirstName());
+
+        System.exit(0);
+
+    }
 }
--- src/main/java/db/DBGolfer.java
+++ src/main/java/db/DBGolfer.java
@@ -1,4 +1,61 @@
 package db;
 
+import models.Golfer;
+import org.hibernate.*;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.List;
+
 public class DBGolfer {
+
+    private static Session session;
+    private static Transaction transaction;
+
+    public static void save(Golfer golfer) {
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.save(golfer);
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public static List<Golfer> getAll(){
+
+        List<Golfer> results = null;
+        session = HibernateUtil.getSessionFactory().openSession();
+
+        try {
+            Criteria cr = session.createCriteria(Golfer.class);
+            results = cr.list();
+        } catch(HibernateException e){
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
+    }
+
+    public static Golfer find(int id){
+
+        Golfer result = null;
+        session = HibernateUtil.getSessionFactory().openSession();
+
+        try {
+            Criteria cr = session.createCriteria(Golfer.class);
+            cr.add(Restrictions.eq("id", id));
+            result = (Golfer)cr.uniqueResult();
+        } catch(HibernateException e){
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return result;
+
+    }
 }
